49567161afb2cb01575a2fee350a460d4893093c
TARUN SINGH <tk452859@gmail.com>
2025-11-28 19:31:12 +0530

    update

--- src/main/java/org/example/billextractor/controller/BillExtractionController.java
+++ src/main/java/org/example/billextractor/controller/BillExtractionController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api") // ← ADD THIS LINE
-
 public class BillExtractionController {
 
 
